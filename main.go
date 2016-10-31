b375cd1f951a92644a34897ad27ffcd4ef6476ca
edwin099 <drop@trashcanmail.com>
2016-10-31 19:50:09 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+// Sync files and directories to and from local and remote object stores
+//
+// Nick Craig-Wood <nick@craig-wood.com>
+heroku buildpacks:set heroku/go
+package main
+
+import (
+	"log"
+
+	"github.com/ncw/rclone/cmd"
+	_ "github.com/ncw/rclone/cmd/all" // import all commands
+	_ "github.com/ncw/rclone/fs/all"  // import all fs
+)
+
+func main() {
+	if err := cmd.Root.Execute(); err != nil {
+		log.Fatalf("Fatal error: %v", err)
+	}
+}
